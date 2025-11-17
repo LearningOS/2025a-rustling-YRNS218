d5a73f33a077eba73b298cdc7ac3639dc034d1df
YRNS218 <ymr20181202@qq.com>
2025-11-17 15:01:03 +0800

    Update intro1.rs

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -1,34 +1,29 @@
-// intro1.rs
-//
-// About this `I AM NOT DONE` thing:
+// intro.rs
+// About this 'I AM NOT DONE' thing:
 // We sometimes encourage you to keep trying things on a given exercise, even
 // after you already figured it out. If you got everything working and feel
-// ready for the next exercise, remove the `I AM NOT DONE` comment below.
-//
+// ready for the next exercise, remove the 'I AM NOT DONE' comment below.
 // If you're running this using `rustlings watch`: The exercise file will be
 // reloaded when you change one of the lines below! Try adding a `println!`
 // line, or try changing what it outputs in your terminal. Try removing a
 // semicolon and see what happens!
-//
-// Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
-// hint.
-
+// Execute `rustlings hint intro` or use the `hint` watch subcommand for a hint.
 
 fn main() {
     println!("Hello and");
-    println!(r#"       welcome to...                      "#);
-    println!(r#"                 _   _ _                  "#);
-    println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
-    println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
-    println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
-    println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
-    println!(r#"                               |___/      "#);
+    println!("welcome to...");
+    println!("                 _   _                 _      _            ");
+    println!("                | | | |               | |    | |           ");
+    println!("                | |_| | ___  _ __ ___ | | __ | |_ ___  ___ ");
+    println!("                |  _  |/ _ \\| '_ ` _ \\| |/ / | __/ _ \\/ __|");
+    println!("                | | | | (_) | | | | | |   <  | ||  __/\\__ \\");
+    println!("                \\_| |_/\\___/|_| |_| |_|_|\\_\\  \\__\\___||___/");
     println!();
     println!("This exercise compiles successfully. The remaining exercises contain a compiler");
     println!("or logic error. The central concept behind Rustlings is to fix these errors and");
     println!("solve the exercises. Good luck!");
     println!();
-    println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
+    println!("The source for this exercise is in 'exercises/intro/intro.rs'. Have a look!");
     println!(
         "Going forward, the source of the exercises will always be in the success/failure output."
     );
