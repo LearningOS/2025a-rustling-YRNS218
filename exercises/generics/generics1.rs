cbf23c4d3600ae8a1618b9d1e71a531a8b39bd8e
YRNS218 <ymr20181202@qq.com>
2025-11-17 14:43:03 +0800

    Update generics1.rs

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -9,6 +9,7 @@
 // I AM NOT DONE
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    // 修复：指定 Vec 的泛型类型为 &str（字符串切片），与后续 push 的值类型匹配
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
