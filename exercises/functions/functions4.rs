4cdd4d9c91d6d260a48708979fc10b4bc41c0e88
YRNS218 <ymr20181202@qq.com>
2025-11-17 14:04:43 +0800

    Update functions4.rs

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -14,6 +14,7 @@ fn main() {
     println!("Your sale price is {}", sale_price(original_price));
 }
 
+// 计算促销价格：接收i32类型价格，返回i32类型促销价
 fn sale_price(price: i32) -> i32 {
     if is_even(price) {
         price - 10
@@ -22,6 +23,7 @@ fn sale_price(price: i32) -> i32 {
     }
 }
 
+// 判断数字是否为偶数：接收i32类型数字，返回bool类型结果
 fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
