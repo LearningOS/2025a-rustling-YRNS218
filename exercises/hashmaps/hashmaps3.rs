3fdb4af8865b951a8183abaa624582cb98c7bae0
YRNS218 <ymr20181202@qq.com>
2025-11-17 14:53:31 +0800

    Update hashmaps3.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -1,19 +1,13 @@
 // hashmaps3.rs
-//
 // A list of scores (one per line) of a soccer match is given. Each line is of
-// the form : "<team_1_name>,<team_2_name>,<team_1_goals>,<team_2_goals>"
+// the form "<team_1>,<team_2>,<team_1_goals>,<team_2_goals>"
 // Example: England,France,4,2 (England scored 4 goals, France 2).
-//
 // You have to build a scores table containing the name of the team, goals the
 // team scored, and goals the team conceded. One approach to build the scores
-// table is to use a Hashmap. The solution is partially written to use a
-// Hashmap, complete it to pass the test.
-//
+// table is to use a HashMap. The solution is partially written to use a
+// HashMap, complete it to pass the tests!
 // Make me pass the tests!
-//
-// Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
-// hint.
-
+// Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 // I AM NOT DONE
 
 use std::collections::HashMap;
@@ -25,7 +19,6 @@ struct Team {
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
-    // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
     for r in results.lines() {
@@ -34,12 +27,30 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        // 处理球队1的数据：更新或插入球队1的得分和失球
+        scores.entry(team_1_name.clone())
+            .and_modify(|t| {
+                t.goals_scored += team_1_score;
+                t.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        // 处理球队2的数据：更新或插入球队2的得分和失球
+        scores.entry(team_2_name.clone())
+            .and_modify(|t| {
+                t.goals_scored += team_2_score;
+                t.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
+
     scores
 }
 
@@ -48,18 +59,16 @@ mod tests {
     use super::*;
 
     fn get_results() -> String {
-        let results = "".to_string()
-            + "England,France,4,2\n"
-            + "France,Italy,3,1\n"
-            + "Poland,Spain,2,0\n"
-            + "Germany,England,2,1\n";
+        let results = "England,france,4,2\n".to_string()
+            + "france,italy,3,1\n"
+            + "poland,spain,2,0\n"
+            + "germany,england,2,1\n";
         results
     }
 
     #[test]
     fn build_scores() {
         let scores = build_scores_table(get_results());
-
         let mut keys: Vec<&String> = scores.keys().collect();
         keys.sort();
         assert_eq!(
@@ -72,15 +81,15 @@ mod tests {
     fn validate_team_score_1() {
         let scores = build_scores_table(get_results());
         let team = scores.get("England").unwrap();
-        assert_eq!(team.goals_scored, 5);
-        assert_eq!(team.goals_conceded, 4);
+        assert_eq!(team.goals_scored, 5); // 4（对法国） + 1（对德国）=5
+        assert_eq!(team.goals_conceded, 4); // 2（对法国） + 2（对德国）=4
     }
 
     #[test]
     fn validate_team_score_2() {
         let scores = build_scores_table(get_results());
         let team = scores.get("Spain").unwrap();
-        assert_eq!(team.goals_scored, 0);
-        assert_eq!(team.goals_conceded, 2);
+        assert_eq!(team.goals_scored, 0); // 对波兰未进球
+        assert_eq!(team.goals_conceded, 2); // 对波兰失2球
     }
 }
