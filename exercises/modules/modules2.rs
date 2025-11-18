d73c4aa41e2abe8ec8d3a313cc33ee8870e6b8fb
YRNS218 <ymr20181202@qq.com>
2025-11-18 10:47:11 +0800

    Update modules2.rs

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,18 +1,14 @@
 // modules2.rs
-//
 // You can bring module paths into scopes and provide new names for them with
 // the 'use' and 'as' keywords. Fix these 'use' statements to make the code
 // compile.
-//
-// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a hint.
 
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // 引入 fruits 模块中的 PEAR 并别名为 fruit
+    use self::fruits::PEAR as fruit;
+    // 引入 veggies 模块中的 CUCUMBER 并别名为 veggie
+    use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
@@ -23,6 +19,10 @@ mod delicious_snacks {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
+
+    // 暴露别名供外部使用（通过 pub use 重导出）
+    pub use self::fruit;
+    pub use self::veggie;
 }
 
 fn main() {
