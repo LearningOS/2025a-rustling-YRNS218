6274fff2041117d958fc6921045a05b55d95d53b
YRNS218 <ymr20181202@qq.com>
2025-11-24 22:02:25 +0800

    Update traits5.rs

--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -1,13 +1,7 @@
 // traits5.rs
-//
-// Your task is to replace the '??' sections so the code compiles.
-//
+// Your task is to replace the '?' sections so the code compiles.
 // Don't change any line other than the marked one.
-//
-// Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint traits5` or use the `hint` watch subcommand for a hint.
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -30,11 +24,11 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+fn some_func(item: &(impl SomeTrait + OtherTrait)) -> bool {
     item.some_function() && item.other_function()
 }
 
 fn main() {
-    some_func(SomeStruct {});
-    some_func(OtherStruct {});
+    some_func(&SomeStruct {});
+    some_func(&OtherStruct {});
 }
