ddf1b3379bcfb1acb8a88c6bee6cfc77a17cf8bc
YRNS218 <ymr20181202@qq.com>
2025-11-24 19:07:04 +0800

    Update primitive_types6.rs

--- exercises/primitive_types/primitive_types6.rs
+++ exercises/primitive_types/primitive_types6.rs
@@ -6,13 +6,12 @@
 // Execute `rustlings hint primitive_types6` or use the `hint` watch subcommand
 // for a hint.
 
-
 #[test]
 fn indexing_tuple() {
     let numbers = (1, 2, 3);
     // Replace below ??? with the tuple indexing syntax.
-    let second = numbers.1;
-
+    let second = numbers.1; // 元组使用点号加索引访问元素，第二个元素索引为1
+    
     assert_eq!(2, second,
         "This is not the 2nd number in the tuple!")
 }
