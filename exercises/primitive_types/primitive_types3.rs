bc958790d02bfbe96380073414639bbcc8e41e10
YRNS218 <ymr20181202@qq.com>
2025-11-24 16:50:01 +0800

    Update primitive_types3.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -1,13 +1,13 @@
 // primitive_types3.rs
 //
-// Create an array with at least 100 elements in it where the ??? is.
+//Create an array with at least 100 elements in it where the ??? is.
 //
-// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
-// for a hint.
-
+//Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
+//for a hint.
 
 fn main() {
-    let a = [0; 1000];
+    let a = [0; 100]; // 使用重复语法创建包含100个0的数组
+    // 也可以用 let a = [0; 101]; 等超过100的长度
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
