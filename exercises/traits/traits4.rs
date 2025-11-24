9d6cbc513608c46714171595e2945aa887f8fe6e
YRNS218 <ymr20181202@qq.com>
2025-11-24 22:01:02 +0800

    Update traits4.rs

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -1,13 +1,8 @@
 // traits4.rs
-//
-// Your task is to replace the '??' sections so the code compiles.
-//
-// Don't change any line other than the marked one.
-//
-// Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+//Your task is to replace the '?' sections so the code compiles.
+//Don't change any line other than the marked one.
+//Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
+//hint.
 
 pub trait Licensed {
     fn licensing_info(&self) -> String {
@@ -23,8 +18,8 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
-    software.licensing_info() == software_two.licensing_info()
+fn compare_license_types(software_one: &dyn Licensed, software_two: &dyn Licensed) -> bool {
+    software_one.licensing_info() == software_two.licensing_info()
 }
 
 #[cfg(test)]
@@ -35,15 +30,13 @@ mod tests {
     fn compare_license_information() {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
-
-        assert!(compare_license_types(some_software, other_software));
+        assert!(compare_license_types(&some_software, &other_software));
     }
 
     #[test]
     fn compare_license_information_backwards() {
         let some_software = SomeSoftware {};
         let other_software = OtherSoftware {};
-
-        assert!(compare_license_types(other_software, some_software));
+        assert!(compare_license_types(&other_software, &some_software));
     }
 }
