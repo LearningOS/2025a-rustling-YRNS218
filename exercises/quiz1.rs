767fe5cfaee7061a234394829fb937a7647e46ed
YRNS218 <ymr20181202@qq.com>
2025-11-25 10:55:57 +0800

    Update quiz1.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -1,26 +1,21 @@
 // quiz1.rs
-//
 // This is a quiz for the following sections:
 // - Variables
 // - Functions
 // - If
-//
 // Mary is buying apples. The price of an apple is calculated as follows:
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given the
 // quantity bought. No hints this time!
-//
 // No hints this time ;)
 
-
-// Put your function here!
+// 实现计算苹果总价的函数
 fn calculate_price_of_apples(number: i32) -> i32 {
-
     if number > 40 {
-        number
+        number * 1  // 超过40个时，每个1rustbuck
     } else {
-        number * 2
+        number * 2  // 不超过40个时，每个2rustbuck
     }
 }
 
@@ -31,7 +26,7 @@ fn verify_test() {
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
-
+    
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(41, price3);
