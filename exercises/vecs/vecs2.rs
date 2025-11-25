bc4ee1a474dc1e613ddcf92e08ca4ca42d800e03
YRNS218 <ymr20181202@qq.com>
2025-11-25 10:54:48 +0800

    Update vecs2.rs

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -1,30 +1,25 @@
 // vecs2.rs
-//
 // A Vec of even numbers is given. Your task is to complete the loop so that
 // each number in the Vec is multiplied by 2.
-//
 // Make me pass the test!
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+        // 对每个元素进行乘2操作（通过可变引用修改原向量）
         *element *= 2;
     }
-
-    // At this point, `v` should be equal to [4, 8, 12, 16, 20].
     v
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
-        // TODO: Do the same thing as above - but instead of mutating the
-        // Vec, you can just return the new number!
-        element * 2
-    }).collect()
+    v.iter()
+        .map(|element| {
+            // 返回每个元素乘2的结果（生成新向量）
+            *element * 2
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -35,7 +30,6 @@ mod tests {
     fn test_vec_loop() {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
         let ans = vec_loop(v.clone());
-
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 
@@ -43,7 +37,6 @@ mod tests {
     fn test_vec_map() {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
         let ans = vec_map(&v);
-
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 }
