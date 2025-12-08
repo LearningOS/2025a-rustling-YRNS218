7a76070db4f978ce9edbebd7c08a6e1ec5eb5a85
YRNS218 <ymr20181202@qq.com>
2025-12-08 19:37:54 +0800

    Update move_semantics5.rs

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -1,17 +1,13 @@
-// move_semantics5.rs
-//
-// Make me compile only by reordering the lines in `main()`, but without adding,
-// changing or removing any of them.
-//
-// Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
     let mut x = 100;
+    
+    // 先使用 y 的可变引用，用完后再创建 z 的可变引用
     let y = &mut x;
     *y += 100;
+
+    // y 的作用域结束后，再创建 z 的可变引用
     let z = &mut x;
     *z += 1000;
+
     assert_eq!(x, 1200);
 }
