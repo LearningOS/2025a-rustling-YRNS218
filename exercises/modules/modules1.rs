5e904bf07d4651367f25179449dbf880c957f085
YRNS218 <ymr20181202@qq.com>
2025-12-08 19:20:23 +0800

    Update modules1.rs

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,17 +1,11 @@
-// modules1.rs
-//
-// Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 mod sausage_factory {
-    // Don't let anybody outside of this module see this!
+    // 保持私有（外部不可见）
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    // 用 pub 修饰，让模块外可以调用
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
