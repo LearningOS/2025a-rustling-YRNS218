52b74cfd14ff98ade0040c21e32f796999924707
YRNS218 <ymr20181202@qq.com>
2025-12-08 19:45:31 +0800

    Update move_semantics6.rs

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -1,27 +1,18 @@
-// move_semantics6.rs
-//
-// You can't change anything except adding or removing references.
-//
-// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data.clone());
+    get_char(&data); // 传递引用，不转移所有权
 
-    string_uppercase(data);
+    string_uppercase(data); // 传递所有权
 }
 
-// Should not take ownership
-fn get_char(data: String) -> char {
+// 不获取所有权，接收引用
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
-// Should take ownership
+// 获取所有权，接收String
 fn string_uppercase(mut data: String) {
-    data.to_uppercase();
-
+    data = data.to_uppercase(); // to_uppercase返回新字符串，重新赋值给data
     println!("{}", data);
 }
