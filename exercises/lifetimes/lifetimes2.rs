640ab6a597ffd0debf15c3c10866cd0a49167bf2
YRNS218 <ymr20181202@qq.com>
2025-12-08 18:39:38 +0800

    Update lifetimes2.rs

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -1,13 +1,4 @@
-// lifetimes2.rs
-//
-// So if the compiler is just validating the references passed to the annotated
-// parameters and the return type, what do we need to change?
-//
-// Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// 修复生命周期标注，并调整变量作用域
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -18,10 +9,8 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 fn main() {
     let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
+    // 将 string2 移到和 result 同作用域（避免内部块销毁 string2）
+    let string2 = String::from("xyz");
+    let result = longest(string1.as_str(), string2.as_str());
     println!("The longest string is '{}'", result);
 }
