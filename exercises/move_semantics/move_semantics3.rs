725d9f05f16e9822d86571eda22653c35c99c732
YRNS218 <ymr20181202@qq.com>
2025-12-08 19:32:21 +0800

    Update move_semantics3.rs

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -1,16 +1,7 @@
-// move_semantics3.rs
-//
-// Make me compile without adding new lines-- just changing existing lines! (no
-// lines with multiple semicolons necessary!)
-//
-// Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0); // 直接接收fill_vec返回的Vec（原vec0的所有权转移后被填充）
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -19,10 +10,9 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> { // 接收Vec所有权，填充后返回
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
     vec
 }
