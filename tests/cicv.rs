bf106730bdd5f93c2f134674ef4f5f6e9590d8ce
YRNS218 <ymr20181202@qq.com>
2025-12-08 20:26:44 +0800

    Update cicv.rs

--- tests/cicv.rs
+++ tests/cicv.rs
@@ -2,11 +2,12 @@ use assert_cmd::prelude::*;
 use std::process::Command;
 
 #[test]
-fn cicvverify() {
+fn cicverify() {
+    // 1. 子命令（cicverify）要放在参数列表的第一个位置
+    // 2. args 接收的是 &[&str] 类型的切片，修正语法格式
     Command::cargo_bin("rustlings")
-        .unwrap()
-        .args(&["--nocapture", "cicvverify"]) 
-        // .current_dir("exercises")
-        .assert()
-        .success();
+       .unwrap()
+       .args(&["cicverify", "--nocapture"]) // 顺序：先子命令，后参数
+       .assert()
+       .success();
 }
