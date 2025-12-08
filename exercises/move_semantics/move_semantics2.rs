cd6aea1f4f52c3241ad61733ddbe680ed49bdde3
YRNS218 <ymr20181202@qq.com>
2025-12-08 19:23:34 +0800

    Update move_semantics2.rs

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,31 +1,23 @@
-// move_semantics2.rs
-//
-// Expected output:
-// vec0 has length 3, with contents `[22, 44, 66]`
-// vec1 has length 4, with contents `[22, 44, 66, 88]`
-//
-// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0.clone());
+    // 直接将 vec0 传入 fill_vec（转移所有权），处理后再取回
+    vec0 = fill_vec(vec0);
 
+    // 此时 vec0 已被 fill_vec 填充内容
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
+    // 将 vec0 的内容克隆到 vec1，再修改 vec1
+    let mut vec1 = vec0.clone();
     vec1.push(88);
 
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+// 接收 Vec 的所有权，填充内容后返回
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
     vec
 }
