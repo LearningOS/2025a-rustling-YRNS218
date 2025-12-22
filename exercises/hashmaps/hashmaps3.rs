b5a48b547cddee2ad4c57ed4f5bfc560c8934e19
YRNS218 <ymr20181202@qq.com>
2025-12-22 20:06:02 +0800

    Update hashmaps3.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -1,14 +1,3 @@
-// hashmaps3.rs
-// A list of scores (one per line) of a soccer match is given. Each line is of
-// the form "<team_1>,<team_2>,<team_1_goals>,<team_2_goals>"
-// Example: England,France,4,2 (England scored 4 goals, France 2).
-// You have to build a scores table containing the name of the team, goals the
-// team scored, and goals the team conceded. One approach to build the scores
-// table is to use a HashMap. The solution is partially written to use a
-// HashMap, complete it to pass the tests!
-// Make me pass the tests!
-// Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
