47e65ac8f1c28511a3b8ca877ccb6a6edd33989b
YRNS218 <ymr20181202@qq.com>
2025-12-22 20:03:40 +0800

    Update generics1.rs

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,12 +1,3 @@
-// generics1.rs
-//
-// This shopping list program isn't compiling! Use your knowledge of generics to
-// fix it.
-//
-// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
 
 fn main() {
     // 修复：指定 Vec 的泛型类型为 &str（字符串切片），与后续 push 的值类型匹配
