994ed2a234f7bdee200e9cff3875a2795619c976
YRNS218 <ymr20181202@qq.com>
2025-12-22 17:23:02 +0800

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,21 @@
-(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-in fetchTarball {
-  url =
-    "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-  sha256 = lock.nodes.flake-compat.locked.narHash;
-}) { src = ./.; }).shellNix
+{
+  # 导入flake.lock并使用flake-compat兼容旧版Nix
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
+
+  outputs = { self, nixpkgs, flake-compat }:
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    {
+      defaultPackage.x86_64-linux = (import (
+        let
+          src = builtins.fetchTarball {
+            url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+            sha256 = lock.nodes.flake-compat.locked.narHash;
+          };
+        in
+        "${src}/shell.nix"
+      ) { inherit nixpkgs; });
+    };
+}
