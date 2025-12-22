a61320545715b5f79f5a1d332c1dc05f45b8434d
YRNS218 <ymr20181202@qq.com>
2025-12-22 17:33:04 +0800

    Update modules3.rs

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -5,12 +5,14 @@
 // you can do it with one line!
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a hint.
 
-// 引入 std::time 模块中的 SystemTime 和 UNIX_EPOCH
+// I AM NOT DONE
+
+// 引入 std::time 模块中的 SystemTime 和 UNIX_EPOCH（一行完成）
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+        Ok(n) => println!("1970-01-01 00:00:00 到现在经过了 {} 秒", n.as_secs()),
+        Err(_) => panic!("时间早于UNIX纪元！"),
     }
 }
