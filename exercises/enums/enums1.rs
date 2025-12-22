8dd21f0b5f5fee27a4828a2cd8208e65237c3533
YRNS218 <ymr20181202@qq.com>
2025-12-22 17:53:00 +0800

    Update enums1.rs

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,19 +2,17 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-       Quit,
-    Echo,
-    Move,
-    ChangeColor,
+    Quit,
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo(String::from("hello")));
+    println!("{:?}", Message::Move { x: 10, y: 20 });
+    println!("{:?}", Message::ChangeColor(255, 0, 0));
 }
