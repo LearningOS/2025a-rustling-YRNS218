fde59a01cb36187a11c9342f62ae29deb471d9b9
YRNS218 <ymr20181202@qq.com>
2025-12-27 18:36:26 +0800

    Update tests6.rs

--- exercises/tests/tests6.rs
+++ exercises/tests/tests6.rs
@@ -1,24 +1,12 @@
-// tests6.rs
-// In this example we take a shallow dive into the Rust standard library's
-// unsafe functions. Fix all the question marks and todos to make the test
-// pass.
-// Execute `rustlings hint tests6` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
 struct Foo {
     a: u128,
     b: Option<String>,
 }
 
-/// # Safety
-/// The `ptr` must contain an owned box of `Foo`.
 unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
-    // SAFETY: The `ptr` contains an owned box of `Foo` by contract. We
-    // simply reconstruct the box from that pointer.
     let mut ret: Box<Foo> = unsafe { Box::from_raw(ptr) };
-    // 补全：修改b字段为Some("hello")
     ret.b = Some("hello".to_owned());
     ret
 }
@@ -27,7 +15,6 @@ unsafe fn raw_pointer_to_box(ptr: *mut Foo) -> Box<Foo> {
 mod tests {
     use super::*;
     use std::time::Instant;
-
     #[test]
     fn test_success() {
         let data = Box::new(Foo { a: 1, b: None });
@@ -35,7 +22,6 @@ mod tests {
         // SAFETY: We pass an owned box of `Foo`.
         let ret = unsafe { raw_pointer_to_box(Box::into_raw(data)) };
         let ptr_2 = &ret.a as *const u128 as usize;
-
         assert!(ptr_1 == ptr_2);
         assert!(ret.b == Some("hello".to_owned()));
     }
