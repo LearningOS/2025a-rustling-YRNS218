7f231ce4efef0421027fb56d9fc53d6ffc8952ca
YRNS218 <ymr20181202@qq.com>
2025-12-27 18:46:27 +0800

    Update options3.rs

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,7 +1,3 @@
-// options3.rs
-// Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -10,12 +6,10 @@ struct Point {
 
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
-
     match y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
 
-    // 修复：使用下划线忽略未使用的变量，避免编译错误
     let _ = y; // Fix without deleting this line.
 }
